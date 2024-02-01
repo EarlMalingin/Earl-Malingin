f745ff6124dd020ef5e1bcda353c3deab46e9158
unknown <Admin@CSLAB221.CSSERVER.local>
2024-02-01 19:35:47 +0800

    ar

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410bf68dcc5f49945dadc81f9d21d27529353b0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee65d8e43f29b70ecba713791ad76d02e5b8301")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
